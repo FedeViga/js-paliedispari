c6e45de625168ab68410d5b6cc80de1f2d5103f2
federico_viganò <fede_viga@hotmail.it>
2024-02-06 17:28:49 +0100

    completato palindroma

--- jsnack2/js/script.js
+++ jsnack2/js/script.js
@@ -6,3 +6,38 @@ Creare una funzione per capire se la parola inserita è palindroma
 ATTENZIONE:
 No .reverse, .split e .join
 */
+
+// inizializzo elementi HTML e variabili
+const inputElement = document.querySelector("#input");
+const submitButtonElement = document.querySelector("#submit");
+const resultElement = document.querySelector("#result");
+let userWord;
+
+// funzione per controllare se una parola è palindroma
+function palindrome (word) {
+
+    // dichiaro la variabile dentro la quale voglio scrivere la parola invertita
+    let reverse = "";
+
+    // inverto word con un ciclo for
+    for (let i = 0; i < word.length; i++) {
+        reverse += word[word.length - 1 - i];
+    }
+
+    // se reverse è uguale a word allora word è palindroma
+    if (word == reverse) {
+        return "La parola " + word + " è Palindroma, infatti letta al contrario rimane " + reverse;
+
+    // altrimenti word non è palindroma
+    } else {
+        return "La parola " + word + " non è Palindroma, infatti letta al contrario è " + reverse;
+    }
+}
+
+// al click del bottone invoco la funzione e stampo il risultato
+submitButtonElement.addEventListener('click', 
+    function () {
+        userWord = inputElement.value;
+        resultElement.innerHTML = palindrome(userWord);
+    }
+)
